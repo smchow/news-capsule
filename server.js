f707c970050ab2f04fb75b680403863fcf3ce14c
Sunita Chowdhury <sunita.m.chowdhury@gmail.com>
2017-04-19 22:59:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,16 @@
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-//var logger = require("morgan");
 var mongoose = require("mongoose");
+
 // Requiring our Comm and Article models
 var Comm = require("./models/Comm.js");
 var Article = require("./models/Article.js");
+
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 var PORT = process.env.PORT||3000;
